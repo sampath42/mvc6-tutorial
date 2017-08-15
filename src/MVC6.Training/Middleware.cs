8af8c2bcff8a41b47a050f09882ceb4df8e3876a
Sampath Kumar <sampath42@gmail.com>
2017-08-15 09:46:52 +0530

    Added Middleware and modified Startup class

---
+++ src/MVC6.Training/Middleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MVC6.Training
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private IFoo _foo;
+
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next, IFoo foo)
+        {
+            _next = next;
+            _foo = foo;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            return httpContext.Response.WriteAsync("In Middleware: " + _foo.GetFoo());
+            //return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
--- src/MVC6.Training/Startup.cs
+++ src/MVC6.Training/Startup.cs
@@ -7,31 +7,85 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace MVC6.Training
 {
+    public interface IFoo { string GetFoo(); }
+    public class Foo : IFoo
+    {
+        MyConfig _config;
+        public Foo(MyConfig config)
+        {
+            _config = config;
+        }
+        public string GetFoo()
+        {
+            return _config.AppName;
+        }
+    }
+
+    public class MyConfig
+    {
+        public string AppName { get; set; }
+    }
+
     public class Startup
     {
+        IConfigurationRoot config;
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+
+            config = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(new MyConfig { AppName = config.GetSection("app:appName").Value });
+            //services.AddSingleton<IFoo, Foo>();
+            services.AddTransient<IFoo, Foo>();
+            //services.AddScoped<IFoo, Foo>();  
+
+            services.AddMvc(options =>
+            {
+                //options.OutputFormatters
+                //options.RespectBrowserAcceptHeader = true;
+                //options.Filters.Add               
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //loggerFactory.AddConsole();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseMiddleware();
+
+            //app.Run(async (context) =>
+            //{
+            //    var foo = context.RequestServices.GetRequiredService<IFoo>();
+            //    await context.Response.WriteAsync(foo.GetFoo());
+            //});
         }
     }
 }
